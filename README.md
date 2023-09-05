9224d9a5ac68c3bccc3cebe59652c095a13c13ad
Renan Tiberto <renan.tiberto@gmail.com>
2023-09-05 20:31:48 -0300

    Configura o Prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto prático para treinar programação baseado na implementação do https://www.tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Bem-vindo a página inicial do meu site. Se você gostou deixe um like!</h1>
+  return (
+    <h1>
+      Bem-vindo a página inicial do meu site. Se você gostou deixe um like!
+    </h1>
+  );
 }
 
 export default Home;
